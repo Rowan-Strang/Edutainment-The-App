8225948ab3def3a638ee1f24284b9d29f517f811
Rowan Strang <rowanstrang@gmail.com>
2025-07-16 12:08:12 +1200

    Initial Commit

---
+++ Edutainment The App/Edutainment_The_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment_The_AppApp.swift
+//  Edutainment The App
+//
+//  Created by MacRow on 16/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Edutainment_The_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
